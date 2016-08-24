acb8ff2a085a721ea9289a51903524e6616c91c7
Nick Mai <nick@pathship.com>
2016-08-25 01:32:54 +0800

    Create test stubs to broadly define behavior

---
+++ main.spec.js
@@ -0,0 +1,27 @@
+'use strict'
+let app = require('./index')
+
+describe('client', () => {
+  it ('should receive messages', () => {
+    
+  })
+  it ('should resolve a change approval properly', () => {
+    
+  })
+  it ('should resolve a conflicting change response', () => {
+    
+  })
+
+  it ('should track revision and pending revision', () => {
+
+  })
+  it ('should send a message with a properly formatted operation', () => {
+
+  })
+
+
+  // optional
+  it ('should aggregate a list of pending operations before sending', () => {
+
+  })
+})
