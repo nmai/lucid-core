86e6fc6414570295ac4f0b591ab233ec77465978
Nick Mai <nick@pathship.com>
2016-10-23 04:01:27 +0800

    removed useless stuff

--- lib/core.ts
+++ lib/core.ts
@@ -1,31 +1,3 @@
-import { BaseOp } from './models/ops'
-
 export class Core {
   constructor() {}
-  
-  process(op: BaseOp) {
-    console.log('asdf')
-
-    let op2 = new BaseOp()
-
-    console.log(op2.constructor)
-    console.log(op2.constructor)
-    
-
-    // hello
-
-    // Insert[0, "heloo"]
-    // Delete[5, 3]
-    // Insert[2, "llo"]
-
-
-    // OperationContainer.push(op2)
-    // OperationContainer.push(opwhatever)
-
-    // OperationContainer.sync()
-
-
-    // ignore
-    return 2
-  }
 }
